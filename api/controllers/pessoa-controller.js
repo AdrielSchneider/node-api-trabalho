22183cd66f65f092aa9b2cb7fe011d8d6c4e9f1c
Adriel Schneider <75347807+AdrielSchneider@users.noreply.github.com>
2022-07-19 16:11:01 -0300

    Implementado controlador de pessoa

--- api/controllers/pessoa-controller.js
+++ api/controllers/pessoa-controller.js
@@ -9,19 +9,66 @@ module.exports = {
       pessoas: pessoas
     });
   },
-  find_by_id: async function(req, res, next) {
-    res.status(200).json({
-      message: "Deve retornar uma única pessoa com base no id"
-    })
+  find_by_id: async function (req, res, next) {
+    try {
+      let pessoa = await PessoaModel.findById(req.params.pessoaId);
+      if (pessoa) {
+        res.status(200).json(pessoa);
+      } else {
+        res.status(400).json('Pessoa não localizada!');
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+
   },
-  register: async function(req, res, next) {
-    res.status(200).json({
-      message: "Deve cadastrar uma nova pessoa no BD"
-    })
+  register: async function (req, res, next) {
+    try {
+
+      let pessoa = new PessoaModel({});
+      pessoa.nome = req.body.nome;
+      pessoa.sobrenome = req.body.sobrenome;
+      pessoa.telefone = req.body.telefone;
+      pessoa.email = req.body.email;
+      pessoa.status = req.body.status;
+
+      pessoa = await pessoa.save();
+
+      res.status(201).json({
+        mensagem: 'Pessoa salva com sucesso',
+        pessoa: {
+          _id: pessoa._id,
+          nome: pessoa.nome,
+          request: {
+            type: 'GET',
+            link: "http://localhost:3000/pessoa/" + pessoa._id
+          }
+        }
+      })
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   },
-  delete_by_id: async function(req, res, next) {
-    res.status(200).json({
-      message: "Deve deletar uma pessoa do BD"
-    })
+  delete_by_id: async function (req, res, next) {
+    const id = req.params.pessoaId;
+    let message = 'Pessoa Excluída com Sucesso';
+    try {
+      let status = await PessoaModel.deleteOne({ _id: id });
+
+      if (status.deletedCount == 0)
+        message = 'Não foi localizada nenhuma pessoa com o Id informado!';
+
+      res.status(200).json({
+        message: message,
+        status: status
+      })
+
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 }
